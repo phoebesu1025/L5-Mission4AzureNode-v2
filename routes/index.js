8e9502665bb2cba00c2314f1b1d7f63f9835251a
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-12-17 13:40:53 +1300

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: "Phoebe's test" });
+  res.render('index', { title: "Phoebe's test GitHub Deployment" });
 });
 
 module.exports = router;
