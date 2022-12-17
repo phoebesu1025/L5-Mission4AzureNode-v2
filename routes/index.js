10d19ff8f910135074cecca9a37d78f50aedcec6
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-12-17 13:51:40 +1300

    Revised the tilte

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: "Phoebe's test GitHub Deployment???" });
+  res.render('index', { title: "Welcome to Turner's Claim History API!" });
 });
 
 module.exports = router;
